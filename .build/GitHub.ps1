c6da01fa5e5f1767471c71185e71b577925ca269
Steve Brown <steve.brown@thebrownnet.co.uk>
2021-03-20 23:56:27 +0000

    Add GitHub build script

---
+++ .build/GitHub.ps1
@@ -0,0 +1,33 @@
+<#
+    Build used in GitHub actions for testing Puppet Modules before release.
+    Does not perform acceptance testing as we take care of that in a separate GitHub action.
+ #>
+[CmdletBinding()]
+param
+(
+    [Parameter(Mandatory = $true)]
+    [string]
+    $ModulePath
+)
+if ($env:CI)
+{
+    $VerbosePreference = "continue"
+}
+try
+{
+    $PuppetModuleToolsPath = (Get-Item $PSScriptRoot -Force).Parent.FullName
+    Import-Module "$PuppetModuleToolsPath/PuppetModuleTools/PuppetModuleTools.psm1" -Force -ErrorAction Stop
+}
+catch
+{
+    throw "Failed to Import PuppetModuleTools PoSh module"
+}
+
+try
+{
+    Test-PuppetModule -ModulePath $ModulePath -TestAcceptance:$false -Verbose
+}
+catch
+{
+    throw "$($_.Exception.Message)"
+}
